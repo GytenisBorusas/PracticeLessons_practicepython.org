81cb9d3030e1c6bff93437f18f40c045393fa66a
Pugilem <gytenis98@gmail.com>
2019-10-27 21:44:58 -0500

    Ex3 done
    Finished Ex3 + comments

--- PracticePython_Ex3.py
+++ PracticePython_Ex3.py
@@ -1,1 +1,21 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# Exercise 3 from https://www.practicepython.org
+
+# A list of elements
+list_a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+list_b = []
+
+number = int(input('Please define the comparison number '))
+
+# Adds an element to the list_a
+list_a.append(99)
+# print(list_a)
+
+# Checks the "list_a" for all the elements that are smaller than "number" and puts them back in the new array called
+# "list_b"
+for element in list_a:
+    if element > number:
+        list_b.append(element)
+
+print(list_b)
+
+
