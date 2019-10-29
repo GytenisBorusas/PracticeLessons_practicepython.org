3105c228ec88e12764631e728b5aa054828b2116
Pugilem <gytenis98@gmail.com>
2019-10-28 20:51:48 -0500

    Ex4 Finished
    Finished and commented

--- PracticePython_Ex4.py
+++ PracticePython_Ex4.py
@@ -7,17 +7,20 @@ divider = number
 # Creates a list of numbers for the output array
 x = range(2,number+1)
 
+# Creates two list. For divisors and not
 list_a = []
-list_b = []
+divisors = []
 
+# Circles through the list checking if its a divisor or not
 for elem in x:
     if number % elem > 0:
-        list_a.append
+        list_a.append(elem)
     else:
-        list_b.append
+        divisors.append(elem)
 
+# Prints the results
 print(list_a)
-print(list_b)
+# print(divisors)
 
 
 
