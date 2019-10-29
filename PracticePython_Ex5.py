2ba83b7a4f05fdda50232dc3081cbc927186e084
Pugilem <gytenis98@gmail.com>
2019-10-28 21:00:10 -0500

    Ex5 Initial commit

---
+++ PracticePython_Ex5.py
@@ -0,0 +1,11 @@
+# Exercise 5 from https://www.practicepython.org
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+list_of_commons = []
+
+if a in b:
+    list_of_commons.append(elem)
+
+print(list_of_commons)
